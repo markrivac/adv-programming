26a17d5ed78e2256f367b8d052f0597710c9addf
Mark <mark.rivac@gmail.com>
2018-04-09 19:27:55 -0500

    Practica 9. C-Database

---
+++ practicas/practica9/write.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct s {
+    char firstName[20];
+    char lastName[20];
+    short Id;
+    char semester[3];
+} Student;
+
+int main(){
+    char *dbName = "student.dat";
+    Student pAvanzada[10];
+
+    for(int i = 0; i < 10; i++){
+        sprintf(pAvanzada[i].firstName, "Name%i", i);
+        sprintf(pAvanzada[i].lastName, "Lastname%i", i);
+        pAvanzada[i].Id = i;
+        sprintf(pAvanzada[i].semester, "S%i", i);
+        
+    }
+
+    FILE *data = fopen(dbName, "wb");
+    fwrite(pAvanzada, sizeof(Student), 10, data);
+    fclose(data);
+    return 0;
+}
